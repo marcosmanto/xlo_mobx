4c54937c17438be3b541a3f50e49d623e4c1d72d
Marcos Mantovani <mantovani.marcos@gmail.com>
2023-01-20 00:45:38 -0300

    Project scaffold with Parse connection test.

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+import 'secret.dart';
+
+void main() async {
+  runApp(const MyApp());
+
+  await Parse().initialize(
+    appId,
+    'https://parseapi.back4app.com/',
+    clientKey: clientKey,
+    autoSendSessionId: true,
+    debug: true,
+  );
+
+  final category = ParseObject('Categories')
+    ..set('Title', 'Camisetas')
+    ..set('Position', 2);
+  final response = await category.save();
+
+  print(response.success);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
