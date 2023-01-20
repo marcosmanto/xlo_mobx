8020d186bc181c9ea90c12f1d1096516ec3952df
Marcos Mantovani <mantovani.marcos@gmail.com>
2023-01-20 13:08:37 -0300

    Added mobx and GetIt to keep singletons of app services. Created PageSore that holds state od current page. BaseScreen have a reaction process that keep listening for changes in PageStore. CustomDrawer > PageSection get the same instance of page store and set page observable according to the tapped button.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 import 'package:xlo_mobx/screens/base/base_screen.dart';
+import 'package:xlo_mobx/stores/page_store.dart';
 import 'secret.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeParse();
+  setupLocators();
   runApp(const MyApp());
 }
 
@@ -17,6 +20,10 @@ Future<void> initializeParse() async {
     autoSendSessionId: true,
     debug: true,
   );
+}
+
+void setupLocators() {
+  GetIt.I.registerSingleton<PageStore>(PageStore());
 }
 
 class MyApp extends StatelessWidget {
--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mobx/mobx.dart';
+import 'package:xlo_mobx/stores/page_store.dart';
 
 import '../home/home_screen.dart';
 
-class BaseScreen extends StatelessWidget {
+class BaseScreen extends StatefulWidget {
   BaseScreen({super.key});
 
+  @override
+  State<BaseScreen> createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
   final PageController pageController = PageController();
+
+  final pageStore = GetIt.I<PageStore>();
+
+  @override
+  void initState() {
+    super.initState();
+    reaction(
+      (_) => pageStore.page,
+      (page) => pageController.jumpToPage(page),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
