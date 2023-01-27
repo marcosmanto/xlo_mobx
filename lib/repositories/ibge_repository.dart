de49299e060d6d255ebcdfba0d826d68ea448807
Marcos Mantovani <mantovani.marcos@gmail.com>
2023-01-27 00:36:06 -0300

    Sorted list alphabetically by name.

--- lib/repositories/ibge_repository.dart
+++ lib/repositories/ibge_repository.dart
@@ -4,13 +4,16 @@ import 'package:xlo_mobx/models/uf.dart';
 class IbgeRepository {
   Future<List<UF>> getUFListFromApi() async {
     const endpoint =
-        'https://servicodados.ibge.gov.br/api/xxv1/localidades/estados';
+        'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
 
     try {
       final response = await Dio().get<List>(endpoint);
       if (response.data != null) {
-        final ufList =
-            response.data!.map<UF>((ufMap) => UF.fromMap(ufMap)).toList();
+        final ufList = response.data!
+            .map<UF>((ufMap) => UF.fromMap(ufMap))
+            .toList()
+          ..sort(
+              (a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
         print(ufList);
         return ufList;
       } else {
