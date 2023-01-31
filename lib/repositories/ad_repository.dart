07c2efa0ac38f010ded428784de1b5f8a3880145
Marcos Mantovani <mantovani.marcos@gmail.com>
2023-01-31 01:29:03 -0300

    Ad is being saved on Parse server.

--- lib/repositories/ad_repository.dart
+++ lib/repositories/ad_repository.dart
@@ -1,7 +1,66 @@
+import 'dart:io';
+
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 import 'package:xlo_mobx/models/ad.dart';
+import 'package:path/path.dart' as path;
+import 'package:xlo_mobx/repositories/parse_errors.dart';
+import 'package:xlo_mobx/repositories/table_keys.dart';
 
 class AdRepository {
   Future<void> save(Ad ad) async {
-    await Future.delayed(Duration(seconds: 5));
+    final parseImages = await saveImages(ad.images);
+
+    final parseUser = ParseUser(null, null, null)..set(keyParseID, ad.user!.id);
+
+    final adObject = ParseObject(keyAdTable);
+
+    final parseACL = ParseACL(owner: parseUser);
+    parseACL.setPublicReadAccess(allowed: true);
+    parseACL.setPublicWriteAccess(allowed: false);
+    adObject.setACL(parseACL);
+
+    adObject.set<String>(keyAdTitle, ad.title);
+    adObject.set<String>(keyAdDescription, ad.description);
+    adObject.set<bool>(keyAdHidePhone, ad.hidePhone);
+    adObject.set<num>(keyAdPrice, ad.price);
+    adObject.set<int>(keyAdStatus, ad.status.index);
+    adObject.set<String>(keyAdDistrict, ad.address.district);
+    adObject.set<String>(keyAdCity, ad.address.city.name);
+    adObject.set<String>(keyAdFederativeUnit, ad.address.uf.initials!);
+    adObject.set<String>(keyAdPostalCode, ad.address.cep);
+    if (parseImages != null) {
+      adObject.set<List<ParseFile>>(keyAdImages, parseImages);
+    }
+    adObject.set<ParseUser>(keyAdOwner, parseUser);
+    adObject.set<ParseObject>(keyAdCategory,
+        ParseObject(keyCategoryTable)..set(keyParseID, ad.category.id));
+    final response = await adObject.save();
+
+    print(response.success);
+  }
+
+  Future<List<ParseFile>?> saveImages(List images) async {
+    final parseImages = <ParseFile>[];
+    try {
+      for (final image in images) {
+        dynamic parseFile;
+        if (image is File) {
+          parseFile = ParseFile(image, name: path.basename(image.path));
+          final response = await parseFile.save();
+          if (!response.success) {
+            Future.error(ParseErrors.getDescription(response.error!.code));
+          }
+        } else {
+          parseFile = ParseFile(null);
+          parseFile.name = path.basename(image);
+          parseFile.url = image;
+        }
+        parseImages.add(parseFile);
+      }
+
+      return parseImages;
+    } catch (e) {
+      Future.error('Falha ao salvar as imagens');
+    }
   }
 }
--- lib/repositories/table_keys.dart
+++ lib/repositories/table_keys.dart
@@ -9,3 +9,18 @@ const keyUserType = 'type';
 
 const keyCategoryTable = 'Categories';
 const keyCategoryDescription = 'description';
+
+const String keyAdTable = 'Ad';
+const String keyAdTitle = 'title';
+const String keyAdDescription = 'description';
+const String keyAdHidePhone = 'hidePhone';
+const String keyAdPrice = 'price';
+const String keyAdStatus = 'status';
+const String keyAdDistrict = 'district';
+const String keyAdCity = 'city';
+const String keyAdPostalCode = 'postalCode';
+const String keyAdFederativeUnit = 'federativeUnit';
+const String keyAdImages = 'images';
+const String keyAdCategory = 'category';
+const String keyAdOwner = 'owner';
+const String keyAdViews = 'views';
